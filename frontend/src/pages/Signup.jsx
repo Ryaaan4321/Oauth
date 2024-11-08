a3a98a5eb4da651159ec4594c6af51d69e51c49d
Ryaaan4321 <aryan056400@gmail.com>
2024-11-08 19:02:26 +0530

    completed the signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [formdata,setformdata]=useState({});
+  const handlechnage=(e)=>{
+   setformdata({...formdata,[e.target.id]:e.target.value});
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7"> Signup</h1>
@@ -10,19 +14,19 @@ export default function Signup() {
           type="text"
           placeholder="username"
           id="username"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg" onChange={handlechnage}
         ></input>
         <input
           type="email"
           placeholder="email"
           id="email"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg" onChange={handlechnage}
         ></input>
         <input
           type="password"
           placeholder="password"
           id="password"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg" onChange={handlechnage}
         ></input>
         <button className="bg-slate-800 rounded-lg uppercase hover:opacity-75 text-white p-3">
           Sign up
