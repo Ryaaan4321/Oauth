a59d93e0a938259110a9be748939896f942de92d
Ryaaan4321 <aryan056400@gmail.com>
2024-11-08 16:11:32 +0530

    the routes are created

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-
+import userroute from './routes/userroute.js'
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("hmlo hmlo from db")
 })
-const app=express();;
-app.listen(3000,()=>{
+const app = express();;
+app.listen(3000, () => {
     console.log("pols aa gyi")
 })
+app.use(express.json())
+app.use('/backend/userroute', userroute);
 
