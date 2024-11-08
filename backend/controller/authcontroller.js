4bb24cfa32639de5ee8ae1dbcbba0ed38de6125d
Ryaaan4321 <aryan056400@gmail.com>
2024-11-08 20:14:59 +0530

    completed the signin page funcionality

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -1,20 +1,42 @@
 import User from '../models/db.js'
-import bcryptjs from 'bcryptjs';
-import { errorhandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs'
+import { errorhandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
     const hashedpassword = bcryptjs.hashSync(password, 10);
     console.log(hashedpassword)
-    const newUser = new User({ username, email, password:hashedpassword });
+    const newUser = new User({ username, email, password: hashedpassword });
     try {
         await newUser.save();
         res.status(201).json({
             msg: "user is created succesfully"
         })
     } catch (err) {
-        next(err); 
+        next(err);
     }
 };
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validuser = await User.findOne({ email: email });
+        if (!validuser) {
+            return next(errorhandler(401, 'User not found'));
+        }
+        const validpassword = bcryptjs.compareSync(password, validuser.password);
+        if (!validpassword) {
+            return next(errorhandler(401, 'Invalid Credentials'));
+        }
+        const token = jwt.sign({ id: validuser._id },
+            process.env.JWT_SECRET
+        )
+        res.status(200).json({
+            token
+        })
+    } catch (error) {
+        next(error);
+    }
+}
