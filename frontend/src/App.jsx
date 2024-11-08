36fba4ae5e80381ccbe311d91ba564cf0e65ee49
Ryaaan4321 <aryan056400@gmail.com>
2024-11-08 15:00:05 +0530

    hmlo hmlo from diffrent account

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className=' text-red-800 flex-row justify-cente'>hmlo hmlo</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    // <h1>Header</h1>
+    <div className="bg-slate-700">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold">
+            <Link to='/'>Auth App</Link>
+        </h1>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/signin">
+          <li>Signin</li></Link>
+        </ul>
+      </div>
+    </div>
+  )
+};
