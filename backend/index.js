6fb38c726c334f71ed4451df04471f8ed3093332
Ryaaan4321 <aryan056400@gmail.com>
2024-11-08 15:33:25 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("hmlo hmlo from db")
+})
 const app=express();;
 app.listen(3000,()=>{
     console.log("pols aa gyi")
 })
+
