57f8ea60383268d57fbbd68f0f0ab10e213059d9
Ryaaan4321 <aryan056400@gmail.com>
2024-11-09 11:11:31 +0530

    added redux toolkit

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -7,63 +7,72 @@ import {
 } from "../redux/user/userslice";
 import { useDispatch, useSelector } from "react-redux";
 
-export default function Signup() {
-  const [formdata, setformdata] = useState({});
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const handlechnage = (e) => {
-    setformdata({ ...formdata, [e.target.id]: e.target.value });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  const handlesubmit = async (e) => {
-    e.preventDefault();
-    dispatch(signinstart());
 
-    const response = await fetch("/backend/auth/signin", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(formdata),
-    });
-    const data = await response.json();
-    if (data.success) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signinstart());
+      const res = await fetch('/backend/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signinfailure(data));
+        return;
+      }
       dispatch(signinsuccess(data));
-      navigate("/");
-    }
-    if (data.success === false) {
-      dispatch(signinfailure());
+      navigate('/');
+    } catch (error) {
+      dispatch(signinfailure(error));
     }
   };
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7"> Signin</h1>
-      <form onSubmit={handlesubmit} className="flex flex-col gap-4">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="email"
-          placeholder="email"
-          id="email"
-          className="bg-slate-100 p-3 rounded-lg"
-          onChange={handlechnage}
-        ></input>
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
         <input
-          type="password"
-          placeholder="password"
-          id="password"
-          className="bg-slate-100 p-3 rounded-lg"
-          onChange={handlechnage}
-        ></input>
-        <button className="bg-slate-800 rounded-lg uppercase hover:opacity-75 text-white p-3">
-          {loading ? "Loading..." : "Sign In"}
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
-      <div className="flex gap-2 mt-1 ">
-        <p>Dont Have an Account?</p>
-        <span className="text-blue-900 ">
-          <Link to="/signup">Sign Up</Link>
-        </span>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/signup'>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
       </div>
-      <p className="text-red-800">{error && "Something went wrong"}</p>
+      <p className='text-red-700 mt-5'>
+        {error ? error.message || 'Something went wrong!' : ''}
+      </p>
     </div>
   );
 }
