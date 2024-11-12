815c470ae1dd2d5b3f41c5718d229080736f97ae
Ryaaan4321 <aryan056400@gmail.com>
2024-11-12 16:06:22 +0530

    hmlooo hmlooo

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const __dirname = path.resolve()
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+const frontendPath = path.join(__dirname, '../frontend/dist')
+app.use(express.static(frontendPath));  
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 });
