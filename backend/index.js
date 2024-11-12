664256ac22c78c31c7c28f59d70a7a576f40cd63
Ryaaan4321 <aryan056400@gmail.com>
2024-11-12 10:53:02 +0530

    completed the backend about to deploy the backend

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,40 @@
 import express from 'express'
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userroute from './routes/userroute.js'
 import authroute from './routes/authroute.js'
-import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("hmlo hmlo from db")
 })
-const app = express();
-app.use(cookieParser());
+
+const __dirname = path.resolve()
+const app = express()
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+});
+
+app.use(cookieParser())
+
 app.listen(3000, () => {
     console.log("pols aa gyi")
 })
+
 app.use(express.json())
 app.use('/backend/user', userroute);
-app.use('/backend/auth',authroute);
+app.use('/backend/auth', authroute);
 
 
-app.use((err,req,res,next)=>{
-    const statuscode=err.statuscode||500;
-    const message=err.message || 'Internal server error';
+
+app.use((err, req, res, next) => {
+    const statuscode = err.statuscode || 500;
+    const message = err.message || 'Internal server error';
     return res.status(statuscode).json({
-        success:false,
+        success: false,
         message,
         statuscode
     });
