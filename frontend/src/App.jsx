021e5a92d736521180a7d2ab0859cb2e0bfac072
Ryaaan4321 <aryan056400@gmail.com>
2024-11-13 16:31:23 +0530

    hmlo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 // import SignIn from "./pages/SignIn.jsx";
+import Login from "./pages/Login.jsx";
 import SignUp from "./pages/Signup.jsx";
 import Profile from "./pages/Profile.jsx";
 import Header from "./components/Header.jsx";
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/signin" element={<SignIn />} /> */}
+        <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
